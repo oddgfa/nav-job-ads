e4916f21ed21c4645c8c685063a74d511ed79113
Odd Gunnar Fatland <oddgunnarfatland@gmail.com>
2023-02-07 15:20:34 +0100

    Add API response

--- src/main/kotlin/com/example/jobads/infrastructure/HttpJobAdsApi.kt
+++ src/main/kotlin/com/example/jobads/infrastructure/HttpJobAdsApi.kt
@@ -1,6 +1,7 @@
 package com.example.jobads.infrastructure
 
 import com.example.jobads.application.KotlinVsJava
+import com.example.jobads.application.KotlinVsJavaCount
 import org.springframework.web.servlet.function.ServerRequest
 import org.springframework.web.servlet.function.ServerResponse
 
@@ -8,8 +9,22 @@ class HttpJobAdsApi(
     private val kotlinVsJava: KotlinVsJava,
 ) {
     fun kotlinVsJava(request: ServerRequest): ServerResponse {
-        kotlinVsJava.kotlinVsJava()
-        return ServerResponse.ok().build()
+        val count = kotlinVsJava.kotlinVsJava()
+        return ServerResponse.ok().body(
+            count.mapValues { KotlinVsJavaResponseJson.fromDomain(it.value) }
+        )
+    }
+}
+
+data class KotlinVsJavaResponseJson(
+    val kotlin: Int,
+    val java: Int,
+) {
+    companion object {
+        fun fromDomain(kotlinVsJava: KotlinVsJavaCount): KotlinVsJavaResponseJson = KotlinVsJavaResponseJson(
+            kotlin = kotlinVsJava.kotlin,
+            java = kotlinVsJava.java,
+        )
     }
 }
 
