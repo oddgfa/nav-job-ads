39548e13d6847bed8eebde9d31637547da20c1e1
Odd Gunnar Fatland <oddgunnarfatland@gmail.com>
2023-02-08 13:00:27 +0100

    Pull out functions

--- src/main/kotlin/com/example/jobads/application/KotlinVsJava.kt
+++ src/main/kotlin/com/example/jobads/application/KotlinVsJava.kt
@@ -14,28 +14,43 @@ class KotlinVsJava(
 
         val count = jobAdsClient.getJobAds(halfAYearAgo, now)
             .fold(mutableMapOf<String, KotlinVsJavaCount>()) { acc, jobAds ->
-                val weekOfYear = WeekFields.of(Locale.getDefault()).weekOfWeekBasedYear()
-                val grouped = jobAds.groupBy {
-                    "${it.published.year}-${it.published.get(weekOfYear)}"
-                }
+                val grouped = groupByWeek(jobAds)
 
+                val count = countKotlinVsJava(grouped)
 
-                val count = grouped.mapValues {
-                    it.value.fold(KotlinVsJavaCount(0, 0)) { acc, jobAd ->
-                        val kotlinCount = if (jobAd.contains("kotlin")) 1 else 0
-                        val javaCount = if (jobAd.contains("java")) 1 else 0
-                        acc + KotlinVsJavaCount(kotlinCount, javaCount)
-                    }
-                }
-
-                count.forEach { (k, v) -> acc.merge(k, v) { oldVal, newVal -> oldVal + newVal } }
-
-                acc
+                mergeCount(count, acc)
             }
 
         println(ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(count))
 
         return count
+    }
+
+    private fun mergeCount(
+        count: Map<String, KotlinVsJavaCount>,
+        acc: MutableMap<String, KotlinVsJavaCount>
+    ): MutableMap<String, KotlinVsJavaCount> {
+        count.forEach { (k, v) -> acc.merge(k, v) { oldVal, newVal -> oldVal + newVal } }
+        return acc
+    }
+
+    private fun countKotlinVsJava(grouped: Map<String, List<JobAd>>): Map<String, KotlinVsJavaCount> {
+        val count = grouped.mapValues {
+            it.value.fold(KotlinVsJavaCount(0, 0)) { acc, jobAd ->
+                val kotlinCount = if (jobAd.contains("kotlin")) 1 else 0
+                val javaCount = if (jobAd.contains("java")) 1 else 0
+                acc + KotlinVsJavaCount(kotlinCount, javaCount)
+            }
+        }
+        return count
+    }
+
+    private fun groupByWeek(jobAds: List<JobAd>): Map<String, List<JobAd>> {
+        val weekOfYear = WeekFields.of(Locale.getDefault()).weekOfWeekBasedYear()
+        val grouped = jobAds.groupBy {
+            "${it.published.year}-${it.published.get(weekOfYear)}"
+        }
+        return grouped
     }
 }
 
